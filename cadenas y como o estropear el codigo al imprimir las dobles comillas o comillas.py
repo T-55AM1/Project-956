e779d54fd2690352109fde86409b64dc1a13fed3
T-55AM1 <tanks.games125@gmail.com>
2026-03-12 22:55:12 -0600

    Add files via upload

---
+++ cadenas y como o estropear el codigo al imprimir las dobles comillas o comillas.py
@@ -0,0 +1,8 @@
+print("I like \"Monty Python\"") #\"texto\" la \" hace que la (") sea valido en el argumento del print 
+print()
+print('I like "Monty Python"')# la (') hace que el (") n confunda el programa
+print()
+print('Soy Monty Python.')
+print()
+print('\'Soy Monty Python\'')
+print("'Soy Monty Python'")
